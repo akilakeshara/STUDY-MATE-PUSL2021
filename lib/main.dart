9070bd751c8d4293165e3d48fcac3f0f163c8df3
ravindu-hash095 <ravindutheekshana025@gmail.com>
2026-03-23 13:30:24 +0530

    Secured Google API Key using dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'screens/auth/welcome_screen.dart';
 import 'screens/auth/login_screen.dart';
@@ -23,7 +24,7 @@ final ValueNotifier<ThemeMode> themeModeNotifier = ValueNotifier(ThemeMode.light
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
+  await dotenv.load(fileName: ".env");
   // Load saved theme preference
   bool isDark = false;
   try {
